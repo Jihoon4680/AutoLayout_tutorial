3f70a0300cc6e785025faaa4a39703981dd0c3e3
JIHOON4680 <jihoon4680@gmail.com>
2022-07-31 20:42:15 +0900

    Basic - Anchor & width,height relation

---
+++ AutoLayout_first/AutoLayout_first/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout_first
+//
+//  Created by 안지훈 on 7/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AutoLayout_tutorial/AutoLayout_tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout_tutorial
+//
+//  Created by 안지훈 on 7/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
